315c37c888be8c68bd04bd3d4ee1b7fe27d11fdf
JontyBurden <azttorrs@gmail.com>
2022-04-24 14:58:07 +1200

    renaming files

---
+++ plugins/mining.py
@@ -0,0 +1,21 @@
+import pyautogui
+import cv2 as cv
+import sys, time
+
+
+# moving the mouse to the upper-left will raise a pyautogui.FailSafeException that can abort the program:
+pyautogui.FAILSAFE = True
+time.sleep(2.5)
+"""
+def mining():
+    mining_position = pag.screenshot('images/mining.png')
+
+
+def check_inv():
+    pass
+
+
+def position_camera():
+    pass
+
+"""
